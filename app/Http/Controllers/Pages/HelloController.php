a7a18a1f38b574484170fe14e6ba0f4240c5d00a
Paweł Tylczyński <pawel.tylczynski@gmail.com>
2021-07-21 20:15:37 +0200

    First commit - instalation, configuration, hello world :)

---
+++ app/Http/Controllers/Pages/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+
+class HelloController extends Controller {
+  public function show() {
+    return view('hello');
+  }
+}
+
+
+
+ ?>
